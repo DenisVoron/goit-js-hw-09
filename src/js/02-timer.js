674fc75868f6810a4e2f5848a48c4ef67df891ed
DenisVoron <Denis.voronko@gmail.com>
2022-08-18 23:41:39 +0300

    make tasks

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,14 @@
-import "flatpickr/dist/flatpickr.min.css";
 import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 console.log(flatpickr);
 
-refs = {
+const refs = {
     input: document.getElementById("datetime-picker"),
     startBtn: document.querySelector('button[data-start]'),
+    day: document.querySelector('span[data-days]'),
+    hour: document.querySelector('span[data-hours]'),
+    minute: document.querySelector('span[data-minutes]'),
+    second: document.querySelector('span[data-seconds]'),
 }
 
 const options = {
@@ -17,7 +21,7 @@ const options = {
     },
 };
 
-//console.log(options.defaultDate);
+console.log(options.defaultDate);
 
 flatpickr(refs.input, options);
 
@@ -25,16 +29,35 @@ flatpickr(refs.input, options);
 const сountdown = {
     start() {
         const startTime = options.defaultDate;
-
+        console.log(startTime);
         setInterval(() => {
             const currentTime = Date.now();
             //console.log('currentTime ->', currentTime);
-
-            //console.log(currentTime - startTime);
+            const deltaTime = currentTime - startTime;
+            const { hours, mins, secs } = getTimeComponents(deltaTime);
+            //console.log(`${hours}:${mins}:${secs}`);
             //console.log(startTime);
+            refs.hour.textContent = `${hours}`;
+            refs.minute.textContent = `${mins}`;
+            refs.second.textContent = `${secs}`;
         }, 1000);
     },
 };
 
-сountdown.start();
+//сountdown.start();
 
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+    const hours = pad(
+        Math.floor((time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)))
+    );
+
+    const mins = pad(Math.floor((time % (1000 * 60 * 60) / (1000 * 60))));
+    const secs = pad(Math.floor((time % (1000 * 60) / 1000)));
+
+    return { hours, mins, secs };
+
+}
