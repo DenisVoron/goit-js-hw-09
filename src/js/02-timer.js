2ba095cf6059247801ba0b16df7abd818a97f1ae
DenisVoron <Denis.voronko@gmail.com>
2022-08-19 17:40:10 +0300

    made task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,8 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-console.log(flatpickr);
 
 const refs = {
-    input: document.getElementById("datetime-picker"),
+    input: document.getElementById('datetime-picker'),
     startBtn: document.querySelector('button[data-start]'),
     day: document.querySelector('span[data-days]'),
     hour: document.querySelector('span[data-hours]'),
@@ -11,53 +10,64 @@ const refs = {
     second: document.querySelector('span[data-seconds]'),
 }
 
+let deltaTime = 0;
+refs.startBtn.disabled = true;
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-        console.log(selectedDates[0]);
-    },
-};
 
-console.log(options.defaultDate);
+    onChange(selectedDates) {
+        if (selectedDates[0] <= Date.now()) {
+            alert('The date cannot be in the past tense');
+        }
+    },
+    onClose(selectedDates) {
+        refs.startBtn.disabled = false;
+
+        deltaTime = selectedDates[0] - Date.now();
+        console.log(deltaTime);
+        refs.startBtn.addEventListener('click', сountdown);
+
+        function сountdown() {
+            const timeId = setInterval((deltaTime) => {
+
+                deltaTime = selectedDates[0] - Date.now();
+
+                if (deltaTime < 1000) {
+                    clearInterval(timeId);
+                }
+
+                const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+
+                refs.day.textContent = days;
+                refs.hour.textContent = hours;
+                refs.minute.textContent = mins;
+                refs.second.textContent = secs;
+
+            }, 1000);
+
+        };
+    },
+
+};
 
 flatpickr(refs.input, options);
-
-
-const сountdown = {
-    start() {
-        const startTime = options.defaultDate;
-        console.log(startTime);
-        setInterval(() => {
-            const currentTime = Date.now();
-            //console.log('currentTime ->', currentTime);
-            const deltaTime = currentTime - startTime;
-            const { hours, mins, secs } = getTimeComponents(deltaTime);
-            //console.log(`${hours}:${mins}:${secs}`);
-            //console.log(startTime);
-            refs.hour.textContent = `${hours}`;
-            refs.minute.textContent = `${mins}`;
-            refs.second.textContent = `${secs}`;
-        }, 1000);
-    },
-};
-
-//сountdown.start();
 
 function pad(value) {
     return String(value).padStart(2, '0');
 }
 
 function getTimeComponents(time) {
+
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = pad(
         Math.floor((time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)))
     );
-
     const mins = pad(Math.floor((time % (1000 * 60 * 60) / (1000 * 60))));
     const secs = pad(Math.floor((time % (1000 * 60) / 1000)));
 
-    return { hours, mins, secs };
-
+    return { days, hours, mins, secs };
 }
