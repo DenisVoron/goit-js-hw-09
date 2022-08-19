da00ef1af1e58971cd35f568ea3b9e5899d73a40
DenisVoron <Denis.voronko@gmail.com>
2022-08-19 23:27:49 +0300

    make task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+const delay = 0;
+const refs = {
+  forms: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name="delay"]'),
+  btn: document.querySelector('button'),
+};
+
+refs.btn.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve('werew');
+      } else {
+        reject('ewqeqe');
+      }
+    }, delay);
+
+  })
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
