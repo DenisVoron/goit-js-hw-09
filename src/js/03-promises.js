9e057232321cf3c7c1b2e6c5a78cedb78412a077
DenisVoron <Denis.voronko@gmail.com>
2022-08-20 14:40:57 +0300

    made task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,70 @@
-const delay = 0;
+import Notiflix from 'notiflix';
+//console.log(Notify);
+
 const refs = {
-  forms: document.querySelector('.form'),
+  form: document.querySelector('.form'),
   inputDelay: document.querySelector('input[name="delay"]'),
   btn: document.querySelector('button'),
 };
 
-refs.btn.addEventListener('submit', createPromise);
+refs.form.addEventListener('submit', onSubmitForm);
+
+let delay = 0;
+let step = 0;
+let amount = 0;
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve('werew');
+        resolve(position, delay);
+        console.log(position);
+        console.log(delay);
       } else {
-        reject('ewqeqe');
+        reject(position, delay);
       }
     }, delay);
 
   })
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+function onSubmitForm(e) {
+  e.preventDefault();
+  //const clickForm = e.currentTarget;
+  //console.log(clickForm);
+  const formData = new FormData(e.currentTarget);
+  console.log(formData);
+
+  formData.forEach((value, name) => {
+    //console.log('onFormSabmit -> value', value);
+    //console.log('onFormSabmit -> name', name);
+    delay = formData.get("delay");
+    step = formData.get("step");
+    amount = formData.get("amount");
+
+    Number(delay);
+
+    console.log(delay);
+    console.log(step);
+    console.log(amount);
+
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, delay)
+        .then((position, delayNum) => {
+          Notiflix.Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          //console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+
+        })
+        .catch((position, delayNum) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          //console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
   });
+  /*const {
+    elements: { delay, step, amount },
+  } = formData;*/
+
+
+}
